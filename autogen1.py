c3e5e2ec1737dbc7707e43804910e24d6aec537b
kaareseras <kaare@seras.dk>
2024-11-27 16:07:37 +0100

    Update autogen1.py

--- autogen1.py
+++ autogen1.py
@@ -14,57 +14,28 @@ config_list_gpt4 = [
     }
 ]
 
-
-gpt4_config = {
-    "seed": 39,  # change the seed for different trials
-    "temperature": 0.5,
-    "config_list": config_list_gpt4,
-    "timeout": 120,
-}
-
+# create an AssistantAgent named "assistant"
+assistant = autogen.AssistantAgent(
+    name="assistant",
+    llm_config={
+        "cache_seed": 42,  # seed for caching and reproducibility
+        "config_list": config_list_gpt4,  # a list of OpenAI API configurations
+        "temperature": 0,  # temperature for sampling
+    },  # configuration for autogen's enhanced inference API which is compatible with OpenAI API
+)
+# create a UserProxyAgent instance named "user_proxy"
 user_proxy = autogen.UserProxyAgent(
-   name="Admin",
-   system_message="A human admin. Interact with the planner to discuss the plan. Plan execution needs to be approved by this admin.",
-   code_execution_config=False,
+    name="user_proxy",
+    human_input_mode="NEVER",
+    max_consecutive_auto_reply=10,
+    is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
+    code_execution_config={
+        "work_dir": "coding",
+        "use_docker": False,  # set to True or image name like "python:3" to use docker
+    },
 )
-cto = autogen.AssistantAgent(
-    name="CTO",
-    system_message="CTO. State your name first. Check the plan and provide feedback from a technical persepctive. Suggest how to build the solution. CTO needs to approve the plan.",
-    llm_config=gpt4_config
-)
- 
-cfo = autogen.AssistantAgent(
-    name="CFO",
-    system_message="CFO. State your name first. Check the plan and provide feedback from a financial persepctive. Suggest how to build the business case. CFO needs to approve the plan.",
-    llm_config=gpt4_config,
-)
- 
-cpo = autogen.AssistantAgent(
-    name="CPO",
-    system_message="CPO. State your name first. Check the plan and provide feedback from a people persepctive. Suggest what capabilities are required to deliver the plan.",
-    llm_config=gpt4_config,
- 
-)
-planner = autogen.AssistantAgent(
-    name="Planner",
-    system_message='''Planner. Suggest a plan. Be Creative within reason.
-      Revise the plan based on feedback from admin, CFO, CPO and critic, until admin approval.
-      Explain the plan first. be clear about the problem, solution, and how to measure success.
-      In the end provide a summary of the plan.
-    ''',
-    llm_config=gpt4_config,
-)
- 
-critic = autogen.AssistantAgent(
-    name="Critic",
-    system_message="Critic. Double check plan, claims, code from other agents and provide feedback. Check whether the plan includes adding verifiable info such as source URL.",
-    llm_config=gpt4_config,
-)
-groupchat = autogen.GroupChat(agents=[user_proxy, cto, cfo, cpo, planner, critic], messages=[], max_round=50)
-manager = autogen.GroupChatManager(groupchat=groupchat, llm_config=gpt4_config)
-
+# the assistant receives a message from the user_proxy, which contains the task description
 user_proxy.initiate_chat(
-    manager,
-message=""" Contoso is a midsize startup in the tech industry. Contoso is looking for the next product to develop and launch in the series of GPT enabled devices. What should this next device be, and what is the plan for development and launch?
-    """,
+    assistant,
+    message="""What date is today? Compare the year-to-date gain for META and TESLA.""",
 )
